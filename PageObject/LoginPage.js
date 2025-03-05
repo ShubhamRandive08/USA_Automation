37fdaefeb5d704d065684d84c5112325e829621c
Shubham B.Randive <srandive245@gmail.com>
2025-03-05 15:29:30 +0530

    register test cases

--- PageObject/LoginPage.js
+++ PageObject/LoginPage.js
@@ -9,6 +9,7 @@ exports.LoginPage = class LoginPage{
         this.page = page
 
         // Element 
+        // Login
         this.popUp = page.locator(".auth-form-light")
         this.heading = page.locator(".auth-form-light > h4")
         this.username = page.locator("//input[@type = 'email']")
@@ -18,6 +19,15 @@ exports.LoginPage = class LoginPage{
         this.loginBtn = page.locator("//a[@id = 'ad-login']")
         this.adminUsername = page.locator("//input[@type = 'text']")
         this.adminLoginBtn = page.locator("//button[@class='swal2-confirm swal2-styled']")
+
+        //Register
+        this.createBtn = page.locator("//a[@id='create-user']")
+        this.heading = page.locator("//h3[@id='welcome']")
+        this.name = page.locator("//input[@id='name']")
+        this.email = page.locator("//input[@id='email']")
+        this.username = page.locator("//input[@id='username']")
+        this.password = page.locator("//input[@id='password']")
+        this.signUpBtn = page.locator("//input[@class='btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn']")
     }
 
     async goTo(){
@@ -33,6 +43,8 @@ exports.LoginPage = class LoginPage{
         await expect(this.createBtn).toBeEnabled()
         await expect(this.loginBtn).toBeEnabled()
     }
+
+    // Login
 
     async teacherLogin(username,password,validateComponents){
         await this.username.click()
@@ -56,5 +68,30 @@ exports.LoginPage = class LoginPage{
         await expect(this.page.locator("//button[@class='swal2-confirm swal2-styled']")).toBeVisible();
         await this.adminLoginBtn.click()
         // await this.page.waitForTimeout(5000)
+    }
+
+    // Register
+    async  registerTeacher(name,email,username,password,validateComponent,clickBtn) {
+        await expect(this.createBtn).toBeVisible()
+        await this.createBtn.click()
+        await expect(this.heading).toBeVisible()
+        await expect(this.name).toBeVisible()
+        await this.name.click()
+        await this.name.fill(name)
+        await expect(this.email).toBeVisible()
+        await this.email.click()
+        await this.email.fill(email)
+        await expect(this.username).toBeVisible()
+        await this.username.click()
+        await this.username.fill(username)
+        await expect(this.password).toBeVisible()
+        await this.password.click()
+        await this.password.fill(password)
+        await expect(this.signUpBtn).toBeVisible()
+        await this.signUpBtn.click()
+        await expect(this.page.locator(`${validateComponent}`)).toBeVisible()
+        await expect(this.page.locator(`${clickBtn}`)).toBeVisible()
+        await this.page.waitForTimeout(1000)
+        await this.page.locator(`${clickBtn}`).click()
     }
 }
---
+++ tests/TeacherRegisterScenarios.spec.js
@@ -0,0 +1,42 @@
+import {test,expect} from '@playwright/test'
+import axios from 'axios'
+const helper = require('../helper')
+const UserCreationData = require('../test-data/UserCreationData.json')
+import { LoginPage } from '../PageObject/LoginPage'
+
+let loginpage;
+
+test.describe('Register New Teacher', async () => {
+    
+    test.beforeEach(async ({ page }) => {
+        loginpage = new LoginPage(page)
+        await loginpage.goTo()
+    })
+
+    test.afterAll(async ({page})=>{
+        await page.close()
+    })
+
+    test("Login With Wrong Email", async ({page}) => {
+        await loginpage.registerTeacher(UserCreationData.wrong_email.name, UserCreationData.wrong_email.email, UserCreationData.wrong_email.username, UserCreationData.wrong_email.password, "//div[@class='swal2-icon swal2-error swal2-icon-show']", "//button[@class='swal2-confirm swal2-styled']")
+    })
+
+    test("Login With Empty Data", async ({page}) => {
+        await loginpage.registerTeacher(UserCreationData.emply_field.name, UserCreationData.emply_field.email, UserCreationData.emply_field.username, UserCreationData.emply_field.password, "//div[@class='swal2-icon swal2-warning swal2-icon-show']", "//button[@class='swal2-confirm swal2-styled']")
+    })
+
+    test("Login With Valid Data", async ({page}) => {
+        await loginpage.registerTeacher(UserCreationData.valid_data.name, UserCreationData.valid_data.email, UserCreationData.valid_data.username, UserCreationData.valid_data.password, "//div[@class='swal2-success-ring']", "//button[@class='swal2-confirm swal2-styled']")
+    })
+
+    test("Validate already register or not", async ({page}) => {
+        await loginpage.registerTeacher(UserCreationData.valid_data.name, UserCreationData.valid_data.email, UserCreationData.valid_data.username, UserCreationData.valid_data.password, "//div[@class='swal2-success-ring']", "//button[@class='swal2-confirm swal2-styled']")
+
+        // Call API for the delete the testuser record from database
+        let res = await axios.get('http://localhost:3000/TeacherNameData')
+
+        await axios.delete("http://localhost:3000/delStaff",{
+            data : {id : res.data.data[0].sid}
+        })
+    })
+})
