d29e7b3686cb57b0087d7f837f36db22908fab25
Shubham Balavant Randive <srandive245@gmail.com>
2025-03-08 12:15:35 +0530

    user2 integration

--- enums.js
+++ enums.js
@@ -2,6 +2,7 @@ const enums = Object.freeze({
     TEACHER_NAME : 'TeacherName',
     TEACHER_NAME_DATA : 'TeacherNameData',
     DEL_STAFF : 'delStaff',
+    STUD_DATA : 'studData',
     VALUE : 'value'
 })
 
